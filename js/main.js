3447342c873be4cc70fead91c853e7043ae4df60
Alextryfg <Alextryfg>
2025-07-14 23:08:30 +0200

    fix: form - falta numero maximo por evento y divider en 3 horas

--- js/main.js
+++ js/main.js
@@ -1,17 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const preloader = document.getElementById("preloader");
-  const hero = document.querySelector(".hero-section");
+  const form = document.getElementById("inscripcion-form");
+  const successOverlay = document.getElementById("form-success");
 
-  setTimeout(() => {
-    preloader.style.opacity = "0";
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    setTimeout(() => {
-      preloader.remove();
-      hero.style.display = "flex";
+    const data = new FormData(form);
+    const json = Object.fromEntries(data.entries());
 
-      // Activa animaciones ya definidas por CSS
-      document.querySelector(".hero-text").classList.add("fade-in-text");
-      document.querySelector(".hero-image").classList.add("fade-in-image");
-    }, 1000);
-  }, 2000); // 2s de preloader antes de mostrar hero
+    fetch("https://hook.eu2.make.com/d1uvsbxvjuh3lp6vpz1f3fsj1rsyxxsw", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(json),
+    })
+      .then((res) => {
+        if (res.ok) {
+          form.reset();
+          successOverlay.classList.add("show");
+
+          setTimeout(() => {
+            successOverlay.classList.remove("show");
+          }, 3000);
+        } else {
+          alert("Error al enviar el formulario.");
+        }
+      })
+      .catch((err) => {
+        console.error("Error:", err);
+        alert("No se pudo enviar el formulario.");
+      });
+  });
 });
